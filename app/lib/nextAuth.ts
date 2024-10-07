569a8d7d80f8568a3ebd83131524df9824965f6e
unknown <hassen.mrakbenjebahi@esprit.tn>
2024-10-07 21:38:53 +0200

    nextauth
    Google provider

---
+++ app/lib/nextAuth.ts
@@ -0,0 +1,19 @@
+import {type AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions:AuthOptions = {
+    providers :[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        })
+    ],
+    session:{
+        strategy:'jwt',
+        maxAge:1 * 24 * 60 * 60, // 1 day
+    },
+    jwt:{},
+    callbacks:{},
+    secret:process.env.NEXTAUTH_SECRET,
+
+};
